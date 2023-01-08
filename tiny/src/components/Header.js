5f4bad19b11fb291bb89fbb0a109264a99105fbe
Monho12 <oorgilmojo@gmail.com>
2023-01-08 18:25:30 +0800

    2nd

--- tiny/src/components/Header.js
+++ tiny/src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthProvider";
 import { RiArrowDownSLine } from "react-icons/ri";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export const Header = () => {
   const { user } = useContext(AuthContext);
@@ -16,16 +17,35 @@ export const Header = () => {
           </Link>
         </div>
         {user && (
-          <div>
-            <Link to="/profile" style={{ textDecoration: "none" }}>
-              <div
-                style={{ color: "black", fontSize: "23px", fontWeight: "700" }}
-              >
-                {user && user.username}
-                <RiArrowDownSLine style={{ color: "#02b589" }} />
-              </div>
-            </Link>
-          </div>
+          // <div>
+          //   <Link to="/profile" style={{ textDecoration: "none" }}>
+          //     <div
+          //       style={{ color: "black", fontSize: "23px", fontWeight: "700" }}
+          //     >
+          //       {user && user.username}
+          //       <RiArrowDownSLine style={{ color: "#02b589" }} />
+          //     </div>
+          //   </Link>
+          // </div>
+          <Dropdown>
+            <Dropdown.Toggle
+              style={{
+                backgroundColor: "transparent",
+                color: "black",
+                fontSize: "23px",
+                fontWeight: "700",
+                border: "none",
+              }}
+              id="dropdown-basic"
+            >
+              {user && user.username}
+              {/* <RiArrowDownSLine style={{ color: "#02b589" }} /> */}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         )}
       </div>
     </div>
